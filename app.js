ee963ccc4b81feea16517b3eb31b9fcbc4320b64
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2022-11-22 15:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function createForm(){
      let form = document.createElement("form");
      let formElements = `<input type="email" id="email" placeholder="email" required><br><br>
                          <input type="tel" id="phone" placeholder="phone" required><br><br>
-                         <button id="submitBtn" type="submit">Purchase</button>`;
+                         <button id="submitBtn" type="submit">Book Now</button>`;
      form.setAttribute("method", "post");
      form.setAttribute("id", "customer-detail-form");
      form.innerHTML = formElements;
